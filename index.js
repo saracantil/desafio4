c92cfecf00c7b27daea68b2218ca04fd5d3c5b1b
SaraCantil <>
2021-05-28 00:39:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var usuario = prompt ("Digite seu login"); 
+var senha = prompt ("Digite sua senha");
+
+  if (usuario == 'teste123') {
+  console.log("usuario correto");
+} else {
+  console.log ("usuario incorreto");
+} if (senha == '1234') {
+  console.log("senha correta"); 
+} else {
+  console.log("senha incorreta");
+}  
+
+// PROGRAMA BASICO DE ENTRADA LOGIN E SENHA #JAVASCRIPT//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
